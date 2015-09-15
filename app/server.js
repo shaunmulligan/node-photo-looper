512041ac0513e32cb06ba5829b10975b114f48c3
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-09-15 15:23:40 -0400

    change x,y pos of image

--- app/server.js
+++ app/server.js
@@ -5,18 +5,20 @@ var fb = pitft("/dev/fb1"); // Returns a framebuffer in direct mode.  See the cl
 // Clear the screen buffer
 fb.clear();
 
-var xMax = fb.size().width;
-var yMax = fb.size().height;
+var xMax = 0; //fb.size().width;
+var yMax = 0; //fb.size().height;
 
 var imageList = ["GE_logo.png", "rpi_logo.png"];
 var imageCounter = 0;
 
 var update = function() {
   if (imageCounter < imageList.length) {
+    fb.clear();
     fb.image(x, y, "images/" + imageList[imageCounter]); // Draw the image at position x, y
     imageCounter++;
   } else {
     imageCounter = 0;
+    fb.clear();
     fb.image(x, y, "images/" + imageList[imageCounter]); // Draw the image from the file "raspberry-pi.png" at position x, y
   }
 }
